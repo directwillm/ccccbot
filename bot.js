28a5d803ff2346f582ea8c7e510f4d5d001a7534
popsiclejokes <directwillm@gmail.com>
2020-08-16 20:59:52 -0400

    fixing index

--- bot.js
+++ bot.js
@@ -15,32 +15,17 @@ client.connect();
 
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+	if (self) { return; } // Ignore messages from the bot
 	console.log(target);
 	console.log(context);
 	console.log(msg);
+	
 	let user = context.username;
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
+	// Remove whitespace from chat message
+	const commandName = msg.trim();
 
-  // If the command is known, let's execute it
-  /*
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else if (commandName === '!cc hurt') {
-	var cmd = new Command('popsiclejokes','hurt 10');
-	currentCommands.push(cmd);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
-  }
-  */
   
-  switch (commandName) {
-		case '!dice':
-			client.say(target, 'You rolled a '+rollDice());
-			break;
+	switch (commandName) {
 		case '!cc hurt':
 			currentCommands.push(new Command(user,'hurt 10'));
 			break;
@@ -53,13 +38,9 @@ function onMessageHandler (target, context, msg, self) {
 		case '!cc laser':
 			currentCommands.push(new Command(user,'laser'));
 			break;
-  }
+	}
 }
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
+
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
